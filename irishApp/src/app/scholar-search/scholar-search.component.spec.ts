9849901ef9e8490ededf56d567d832682968e3d7
Nathan Dunne <72324104+TrainCoillte@users.noreply.github.com>
2022-02-28 21:17:38 +0000

    Update scholar-search.component.spec.ts

--- irishApp/src/app/scholar-search/scholar-search.component.spec.ts
+++ irishApp/src/app/scholar-search/scholar-search.component.spec.ts
@@ -4,6 +4,7 @@ import { ScholarSearchComponent } from './scholar-search.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DataService } from "../data.service";
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from '../footer/footer.component';
 describe('ScholarSearchComponent', () => {
   let component: ScholarSearchComponent;
   let fixture: ComponentFixture<ScholarSearchComponent>;
@@ -11,7 +12,7 @@ describe('ScholarSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ ScholarSearchComponent ]
+      declarations: [ ScholarSearchComponent ,FooterComponent]
     })
     .compileComponents();
   });
@@ -20,8 +21,7 @@ describe('ScholarSearchComponent', () => {
     fixture = TestBed.createComponent(ScholarSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    var scribes=[{Ainm: "Criostóir Aindreamha ",
+    component.scribes[0] = {Ainm: "Criostóir Aindreamha ",
     Breise: "",
     DataBais: "1860-11-25T00:25:00.000Z",
     DataBreithe: "1764-00-00",
@@ -41,27 +41,7 @@ describe('ScholarSearchComponent', () => {
     Seoladh8: "N/A",
     Seoladh9: "N/A",
     Seoladh10: "",
-    SliBheatha: "Hucstaeir "}];
-
-    var form = {Ainm: "",
-    Contae: "",
-    DataBais: "",
-    DataBreithe: "",
-    FloruitEnd: "",
-    FloruitStart: "",
-    Lamhscribhinni: "",
-    Patrun: "",
-    SliBheatha: ""};
-
-    var formNull = {Ainm: null,
-      Contae:null,
-      DataBais: null,
-      DataBreithe: null,
-      FloruitEnd: null,
-      FloruitStart: null,
-      Lamhscribhinni: null,
-      Patrun: null,
-      SliBheatha: null};
+    SliBheatha: "Hucstaeir "};
   });
   
 
@@ -71,13 +51,79 @@ describe('ScholarSearchComponent', () => {
   it('should test the form submission', () => {
     component.onSubmit()
     expect(component.showStringbool).toEqual(false);
-    console.log(component.profileForm);
-    console.log(component.scribe);
-    console.log(component.scribes);
   });
-  it('should test the data given and change values', () => {
-
+  it('should test the form submission if values are null', () => {
+    component.profileForm.value.Ainm=null;
+    component.profileForm.value.DataBreitheó=null;
+    component.profileForm.value.DataBreithechun=null;
+    component.profileForm.value.DataBaisó=null;
+    component.profileForm.value.DataBaischun=null;
+    component.profileForm.value.Patrun=null;
+    component.profileForm.value.Lamhscribhinni=null;
+    component.profileForm.value.FloruitStartó=null;
+    component.profileForm.value.FloruitStartchun=null;
+    component.profileForm.value.FloruitEndó=null;
+    component.profileForm.value.FloruitEndchun=null;
+    component.profileForm.value.SliBheatha=null;
+    component.profileForm.value.Contae=null;
+    component.onSubmit();
+    expect(component.scribe).toEqual(component.scribes);
+  });
+  it('should test that county name changes', () => {
+    component.profileForm.value.Contae='Doire';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Dhoire");
+    component.profileForm.value.Contae='Aontroim';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Aontroma");
+    component.profileForm.value.Contae='An Cabhán';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Chabháin");
+    component.profileForm.value.Contae='An Dún';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Dúin");
+    component.profileForm.value.Contae='Dún na nGall';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Dhún na nGall");
+    component.profileForm.value.Contae='Fear Manach';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Fhear Manach");
+    component.profileForm.value.Contae='Muineachán';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Mhuineacháin");
+    component.profileForm.value.Contae='Tír Eoghain';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Thír Eoghain");
+    component.profileForm.value.Contae='An Iarmhí';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Iarmhí");
+    component.profileForm.value.Contae='An Lú';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Lú");
+    component.profileForm.value.Contae='An Mhí';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Mí");
+    component.profileForm.value.Contae='Cill Dara';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Chill Dara");
+    component.profileForm.value.Contae='Longfort';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Longfoirt");
+    component.profileForm.value.Contae='Liatroim';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Liatroma");
+    component.profileForm.value.Contae='Maigh Eo';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Mhaigh Eo");
+    component.profileForm.value.Contae='Sligeach';
+    component.onSubmit();
+    expect(component.profileForm.value.Contae).toEqual( "Shligigh");
    });
+   it('should test that if name is entered, it searches, fills correctly and ignores case', () => {
+    component.profileForm.value.Ainm = 'Criostóir';
+    component.onSubmit();
+    expect(component.scribe.length).toEqual(1);
+  });
   // it('should test if values match', () => {
   //   expect(component.showStringbool).toEqual(false);
   //   component.scribe = [{Ainm: "Criostóir Aindreamha ",
